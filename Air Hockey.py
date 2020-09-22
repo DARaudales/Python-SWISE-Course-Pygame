c4f2c77153abb29fba7eabb0495fadbb4b1160e3
Maria Renee Meza <70554010+mariareneemf@users.noreply.github.com>
2020-09-22 01:58:13 -0600

    Update Air Hockey.py

--- Air Hockey.py
+++ Air Hockey.py
@@ -40,3 +40,24 @@ class Pelota(pygame.sprite.Sprite):
         self.dy = 1
 
 
+barra1 = Barra1()
+barra1.rect.x = 25
+barra1.rect.y = 225
+
+barra2= Barra2()
+barra2.rect.x = 715
+barra2.rect.y = 225
+
+velocidad_barra = 10
+
+ball = Pelota()
+ball.rect.x = 375
+ball.rect.y = 250
+
+# Grupo de sprites
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(barra1, barra2, ball)
+
+
+
